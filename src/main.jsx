1e144148eb1737e58f8ef49ab70a8470fe2ceb73
Shiven Kathuria <shiven.25bcs10290@sst.scaler.com>
2026-07-15 16:59:42 +0530

    fixed deployment font issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/500.css";
+import "@fontsource/geist-sans/600.css";
+import "@fontsource/geist-sans/700.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
